5a8312069a27100210bbee7489c6e50fabba55ce
Gabriel Chazarreta <gfchazarreta@gmail.com>
2022-06-09 23:41:47 -0300

    Adding HashRouter instead of BrowserRouter

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { App } from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='whos-that-pokemon'>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
